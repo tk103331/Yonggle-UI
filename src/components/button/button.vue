fc6ada458deba8875603b1f610e741ca155d9581
tk103331 <tk103331@gmail.com>
2019-01-21 08:57:17 +0800

    Button

---
+++ src/components/button/button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button :type="nativeType" :class="buttonClass" :role="role" :disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'YgButton',
+  props: {
+    nativeType: String,
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: String,
+    role: String,
+    active: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    block: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    buttonClass() {
+      return 'btn' + (this.type ? ' btn-' + this.type : '') + (this.active ? ' active' : '')
+              + (this.block ? ' btn-block' : '') + (this.size ? ' btn-' + this.size : '');
+    }
+  }
+}
+</script>
+
+
---
+++ src/components/button/index.js
@@ -0,0 +1,7 @@
+import YgButton from './button.vue';
+
+YgButton.install = function(Vue) {
+  Vue.component(YgButton.name, YgButton);
+};
+
+export default YgButton;
