4fb0e6318d9a75ce2767126602715ad9f776add9
tk103331 <tk103331@gmail.com>
2019-01-21 09:00:04 +0800

    Image

---
+++ src/components/image/image.vue
@@ -0,0 +1,25 @@
+<template>
+  <img :src="src" :alt="alt" :title="title" :class="imgClass">
+</template>
+
+<script>
+export default {
+  name: 'YgImage',
+  props: {
+    src: String,
+    alt: String,
+    title: String,
+    type: String,
+    responsive: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    imgClass() {
+      return 'img' + (this.responsive ? ' img-responsive' : '') + (this.type ? ' img-' + this.type : '');
+    }
+  }
+}
+</script>
+
---
+++ src/components/image/index.js
@@ -0,0 +1,7 @@
+import YgImage from './image.vue';
+
+YgImage.install = function(Vue) {
+  Vue.component(YgImage.name, YgImage);
+};
+
+export default YgImage;
