a7f4081a9b38c7b348a44f8239e31e883af4c68c
tk103331 <tk103331@gmail.com>
2019-01-23 19:40:51 +0800

    test

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,14 +1,83 @@
 <template>
-  <l-container :fluid="true">
-    <l-row>
-      <l-col :md="1" :offset="2" :pull="3" :push="4">
-        col-md-1 col-md-offset-2 col-md-pull-3 col-md-push-4
-      </l-col>
-    </l-row>
-    <l-row>
+  <yg-container :fluid="true">
+    <yg-row><h2>栅格系统</h2></yg-row>
+    <yg-row class="show-grid">
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+      <yg-col :md="1">.col-md-1</yg-col>
+    </yg-row>
+    <yg-row class="show-grid">
+      <yg-col :md="8">.col-md-8</yg-col>
+      <yg-col :md="4">.col-md-4</yg-col>
+    </yg-row>
+    <yg-row class="show-grid">
+      <yg-col :md="6">.col-md-6</yg-col>
+      <yg-col :md="6">.col-md-6</yg-col>
+    </yg-row>
+    <yg-row class="show-grid">
+      <yg-col :md="3">.col-md-3</yg-col>
+      <yg-col :md="3" :offset="3">.col-md-3</yg-col>
+    </yg-row>
 
-    </l-row>
-  </l-container>
+    <yg-row><h2>按钮</h2></yg-row>
+    <yg-row><h5>按钮样式</h5></yg-row>
+    <yg-row>
+      <yg-button type='default'>（默认样式）Default</yg-button>
+      <yg-button type='primary'>（首选项）Primary</yg-button>
+      <yg-button type='success'>（成功）Success</yg-button>
+      <yg-button type='info'>（一般信息）Info</yg-button>
+      <yg-button type='warning'>（警告）Warning</yg-button>
+      <yg-button type='danger'>（危险）Danger</yg-button>
+      <yg-button type='link'>（链接）Link</yg-button>
+    </yg-row>
+    <yg-row><h5>按钮大小</h5></yg-row>
+    <yg-row>
+      <yg-button type='primary' size="lg">（大按钮）Large button</yg-button>
+      <yg-button type='default' size="lg">（大按钮）Large button</yg-button>
+    </yg-row>
+    <yg-row>
+      <yg-button type='primary'>（默认尺寸）Default button</yg-button>
+      <yg-button type='default'>（默认尺寸）Default button</yg-button>
+    </yg-row>
+    <yg-row>
+      <yg-button type='primary' size="sm">（小按钮）Small button</yg-button>
+      <yg-button type='default' size="sm">（小按钮）Small button</yg-button>
+    </yg-row>
+    <yg-row>
+      <yg-button type='primary' size="xs">（超小尺寸）Extra small button</yg-button>
+      <yg-button type='default' size="xs">（超小尺寸）Extra small button</yg-button>
+    </yg-row>
+    <yg-row>
+      <yg-button type='primary' size="lg" block>（块级元素）Block level button</yg-button>
+      <yg-button type='default' size="lg" block>（块级元素）Block level button</yg-button>
+    </yg-row>
+    <yg-row><h5>激活状态</h5></yg-row>
+    <yg-row>
+      <yg-button type='primary' size="lg" active>激活状态</yg-button>
+      <yg-button type='default' size="lg" active>激活状态</yg-button>
+    </yg-row>
+    <yg-row><h5>禁用状态</h5></yg-row>
+    <yg-row>
+      <yg-button type='primary' size="lg" disabled>禁用状态</yg-button>
+      <yg-button type='default' size="lg" disabled>禁用状态</yg-button>
+    </yg-row>
+
+    <yg-row><h2>图片</h2></yg-row>
+    <yg-row class="show-image">
+      <yg-image src="/static/img/img1.svg" title="img-rounded" type="rounded"></yg-image>
+      <yg-image src="/static/img/img1.svg" title="img-circle" type="circle"></yg-image>
+      <yg-image src="/static/img/img1.svg" title="img-thumbnail" type="thumbnail"></yg-image>
+    </yg-row>
+  </yg-container>
 </template>
 
 <script>
@@ -16,19 +85,41 @@ import YgContainer from "@/components/container";
 import YgRow from "@/components/row";
 import YgCol from "@/components/col";
 import YgTable from "@/components/table";
+import YgButton from "@/components/button";
+
+import YgImage from "@/components/image";
 
 
 export default {
+  data:{
+    img1: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNjg2OWY2MGE4NyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE2ODY5ZjYwYTg3Ij48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQ0LjA1NDY4NzUiIHk9Ijc0LjUiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4='
+  },
   components: {
-    LContainer,
-    LRow,
-    LCol,
-    LTable,
+    YgContainer,
+    YgRow,
+    YgCol,
+    YgTable,
+    YgButton,
+    YgImage,
   }
 }
 </script>
 
 <style>
-
+.show-grid [class^=col-] {
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #eee;
+    background-color: rgba(86,61,124,.15);
+    border: 1px solid #ddd;
+    border: 1px solid rgba(86,61,124,.2);
+}
+.row{
+  margin: 5px;
+}
+.row.show-image>img{
+  width: 140px;
+  height: 140px;
+}
 </style>
 
